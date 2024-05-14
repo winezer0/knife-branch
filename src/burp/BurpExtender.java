b6ce68415b5868efe55e3e909d5cc073af60af88
WINEZERO <kissloo@foxmail.com>
2024-05-14 15:07:11 +0800

    还原主分支修改

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 import com.bit4woo.utilbox.burp.HelperPlus;
 import com.google.gson.Gson;
@@ -22,7 +23,6 @@ import messageTab.Info.InfoTabFactory;
 import messageTab.U2C.ChineseTabFactory;
 import config.ProcessManager;
 import org.apache.commons.lang3.StringUtils;
-import plus.*;
 
 public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFactory, ITab, IHttpListener, IProxyListener, IExtensionStateListener {
 
@@ -30,26 +30,24 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
      *
      */
     private static final long serialVersionUID = 1L;
-    
+
     public static IBurpExtenderCallbacks callbacks;
     public static IExtensionHelpers helpers;
     private static HelperPlus helperPlus;
-
-
-​    
+    
+    
     public static PrintWriter stdout;
     public static PrintWriter stderr;
     public IContextMenuInvocation invocation;
 
-
-​	
+	
     public static String ExtensionName = "Knife";
     public static String Version = bsh.This.class.getPackage().getImplementationVersion();
     public static String Author = "by bit4woo";
     public static String github = "https://github.com/bit4woo/knife";
-    
+
     public static String CurrentProxy = "";
-    
+
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         BurpExtender.callbacks = callbacks;
@@ -59,40 +57,32 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
         flushStd();
         BurpExtender.stdout.println(getFullExtensionName());
         BurpExtender.stdout.println(github);
-    
-        // [重要] 使用 SwingUtilities.invokeLater 解决操作过快 导致出现swing崩溃的问题
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                configTable = new ConfigTable(new ConfigTableModel());
-                configPanel.setViewportView(configTable);
-    
-                String content = callbacks.loadExtensionSetting("knifeconfig");
-                if (StringUtils.isEmpty(content)) {
-                    content = initConfig();
-                }
-    
-                configManager = new Gson().fromJson(content, ConfigManager.class);
-                showToUI(configManager);
-    
-                ChineseTabFactory chntabFactory = new ChineseTabFactory(null, false, helpers, callbacks);
-    
-                //各项数据初始化完成后在进行这些注册操作，避免插件加载时的空指针异常
-                callbacks.setExtensionName(getFullExtensionName());
-                callbacks.registerContextMenuFactory(BurpExtender.this);// for menus
-                callbacks.registerMessageEditorTabFactory(chntabFactory);// for Chinese
-                callbacks.addSuiteTab(BurpExtender.this);
-                callbacks.registerHttpListener(BurpExtender.this);
-                callbacks.registerProxyListener(BurpExtender.this);
-                callbacks.registerExtensionStateListener(BurpExtender.this);
-    
-                //自动加载用户指定的 Project Json文件,如果不存在会自动保存当前配置
-                AdvScopeUtils.autoLoadProjectConfig(callbacks);
-                //追加用户设置的默认需要排除的数据
-                AdvScopeUtils.addDefaultExcludeHosts(callbacks);
-    
-                BurpExtender.stdout.println("Load Extension Success ...");
-            }
+
+        configTable = new ConfigTable(new ConfigTableModel());
+        configPanel.setViewportView(configTable);
+
+        String content = callbacks.loadExtensionSetting("knifeconfig");
+        if (StringUtils.isEmpty(content)) {
+            content = initConfig();
         }
+
+        configManager = new Gson().fromJson(content, ConfigManager.class);
+        showToUI(configManager);
+
+        ChineseTabFactory chntabFactory = new ChineseTabFactory(null, false, helpers, callbacks);
+        InfoTabFactory infotabFactory = new InfoTabFactory(null, false, helpers, callbacks);
+
+        //各项数据初始化完成后在进行这些注册操作，避免插件加载时的空指针异常
+        callbacks.setExtensionName(getFullExtensionName());
+        callbacks.registerContextMenuFactory(this);// for menus
+        callbacks.registerMessageEditorTabFactory(chntabFactory);// for Chinese
+        callbacks.registerMessageEditorTabFactory(infotabFactory);// for Chinese
+        callbacks.addSuiteTab(BurpExtender.this);
+        callbacks.registerHttpListener(this);
+        callbacks.registerProxyListener(this);
+        callbacks.registerExtensionStateListener(this);
+    }
+
 
     private static void flushStd() {
         try {
@@ -103,55 +93,46 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
             stderr = new PrintWriter(System.out, true);
         }
     }
-    
+
     public static PrintWriter getStdout() {
         flushStd();//不同的时候调用这个参数，可能得到不同的值
         return stdout;
     }
-    
+
     public static PrintWriter getStderr() {
         flushStd();
         return stderr;
     }
-    
+
     //name+version+author
     public static String getFullExtensionName() {
         return ExtensionName + " " + Version + " " + Author;
     }
-    
+
     //JMenu 是可以有下级菜单的，而JMenuItem是不能有下级菜单的
     @Override
     public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
         ArrayList<JMenuItem> menu_item_list = new ArrayList<JMenuItem>();
-    
+
         this.invocation = invocation;
         //常用
         menu_item_list.add(new OpenWithBrowserMenu(this));
         menu_item_list.add(new CustomPayloadMenu(this));
         menu_item_list.add(new CustomPayloadForAllInsertpointMenu(this));
-    
+
         //cookie身份凭证相关
         menu_item_list.add(new UpdateCookieMenu(this));
         menu_item_list.add(new UpdateCookieWithHistoryMenu(this));
         menu_item_list.add(new ChangeToUploadRequest(this));
-    
+
         menu_item_list.add(new SetCookieMenu(this));
         menu_item_list.add(new SetCookieWithHistoryMenu(this));
-    
+
         UpdateHeaderMenu updateHeader = new UpdateHeaderMenu(this);//JMenuItem vs. JMenu
         if (updateHeader.getItemCount() > 0) {
             menu_item_list.add(updateHeader);
         }
-    
-        //winzer0 添加 配置文件相关 //手动更新用户指定的 Project Json 文件
-        menu_item_list.add(new ProjectConfigLoadMenu(this));
-        menu_item_list.add(new ProjectConfigSaveMenu(this));
-        menu_item_list.add(new ProjectScopeClearMenu(this));
-        menu_item_list.add(new AddHostToInScopeMenu(this));
-        menu_item_list.add(new AddHostToInScopeAdvMenu(this));
-        menu_item_list.add(new AddHostToExScopeMenu(this));
-        menu_item_list.add(new AddHostToExScopeAdvMenu(this));
-    
+
         //扫描攻击相关
         menu_item_list.add(new AddHostToScopeMenu(this));
         menu_item_list.add(new RunCmdMenu(this));
@@ -161,17 +142,17 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
         //不太常用的
         menu_item_list.add(new DismissMenu(this));
         menu_item_list.add(new DismissCancelMenu(this));
-    
+
         menu_item_list.add(new ChunkedEncodingMenu(this));
         menu_item_list.add(new DownloadResponseMenu(this));
         //menu_item_list.add(new DownloadResponseMenu2(this));
         //menu_item_list.add(new ViewChineseMenu(this));
         //menu_item_list.add(new JMenuItem());
         //空的JMenuItem不会显示，所以将是否添加Item的逻辑都方法到类当中去了，以便调整菜单顺序。
-    
+
         menu_item_list.add(new CopyJsOfThisSite(this));
         menu_item_list.add(new FindUrlAndRequest(this));
-    
+
         Iterator<JMenuItem> it = menu_item_list.iterator();
         while (it.hasNext()) {
             JMenuItem item = it.next();
@@ -179,7 +160,7 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
                 it.remove();
             }
         }
-    
+
         String oneMenu = configTableModel.getConfigValueByKey("Put_MenuItems_In_One_Menu");
         if (oneMenu != null) {
             ArrayList<JMenuItem> Knife = new ArrayList<JMenuItem>();
@@ -205,19 +186,19 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
     public Component getUiComponent() {
         return this.getContentPane();
     }
-    
+
     @Override
     public void extensionUnloaded() {
         saveConfigToBurp();
     }
-    
+
     @Override
     public String initConfig() {
         configManager = new ConfigManager("default");
         configTableModel = new ConfigTableModel();
         return getAllConfig();
     }
-    
+
     //IProxyListener中的方法，修改的内容会在proxy中显示为edited
     @Override
     public void processProxyMessage(boolean messageIsRequest, IInterceptedProxyMessage message) {
@@ -232,14 +213,14 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
         for (ConfigEntry  rule:rules){
             rule.takeProxyAction(messageIsRequest,message);
         }
-    
+
         if (messageIsRequest) {
             if (isInCheckBoxScope(IBurpExtenderCallbacks.TOOL_PROXY, messageInfo)) {
                 ProcessManager.doChunk(messageIsRequest, messageInfo);
             }
         }
     }
-    
+
     //IHttpListener中的方法，修改的内容在Proxy中不可见
     @Override
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
@@ -254,11 +235,11 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
                 } else {
                     List<ConfigEntry> rules = ProcessManager.getEditActionRules();
                     for (int index = rules.size() - 1; index >= 0; index--) {//按照时间倒叙引用规则
-    
+
                         ConfigEntry rule = rules.get(index);
                         rule.takeEditAction(toolFlag, messageIsRequest, messageInfo);
                     }
-    
+
                     if (isInCheckBoxScope(toolFlag, messageInfo)) {
                         ProcessManager.doChunk(messageIsRequest, messageInfo);
                     }
@@ -269,28 +250,28 @@ public class BurpExtender extends GUI implements IBurpExtender, IContextMenuFact
             e.printStackTrace(stderr);
         }
     }
-    
+
     public static IBurpExtenderCallbacks getCallbacks() {
         return callbacks;
     }
 
 
     public static HelperPlus getHelperPlus() {
-    	return helperPlus;
-    }
+		return helperPlus;
+	}
 
 
 	public static boolean isInCheckBoxScope(int toolFlag, IHttpRequestResponse messageInfo) {
-	    if (toolFlag == (toolFlag & configManager.getEnableStatus())) {
-	
-	        IExtensionHelpers helpers = getCallbacks().getHelpers();
-	        URL url = new HelperPlus(helpers).getFullURL(messageInfo);
-	
-	        if (!configManager.isOnlyForScope() || callbacks.isInScope(url)) {
-	            return true;
-	        }
-	    }
-	    return false;
-	}
+        if (toolFlag == (toolFlag & configManager.getEnableStatus())) {
+
+            IExtensionHelpers helpers = getCallbacks().getHelpers();
+            URL url = new HelperPlus(helpers).getFullURL(messageInfo);
+
+            if (!configManager.isOnlyForScope() || callbacks.isInScope(url)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
